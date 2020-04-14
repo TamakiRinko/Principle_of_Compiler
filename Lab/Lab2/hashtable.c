27d3174a1d77da6eecd505258059cca5a0d059cf
TamakiRinko <1957880233@qq.com>
2020-04-15 01:29:32 +0800

    添加至funDec分析

--- Lab/Lab2/hashtable.c
+++ Lab/Lab2/hashtable.c
@@ -2,6 +2,15 @@
 
 Symbol symbolTable[TABLE_NUM];
 
+Symbol newSymbol(char* name, Type type, int lineno){
+    Symbol symbol = (Symbol)malloc(sizeof(struct Symbol_));
+    symbol->name = (char*)malloc(strlen(name) + 1);
+    strcpy(symbol->name, name);
+    symbol->type = type;
+    symbol->lineno = lineno;
+    return symbol;
+}
+
 unsigned int hash_pjw(char* name){
     unsigned int val = 0, i;
     for (; *name; ++name){
@@ -9,6 +18,46 @@ unsigned int hash_pjw(char* name){
         if (i = val & ~0x3fff) val = (val ^ (i >> 12)) & TABLE_NUM;
     }
     return val;
+}
+
+int isEqual(Type t1, Type t2){
+    if(t1->kind != t2->kind){
+        return 0;
+    }
+    switch(t1->kind){
+        case BASIC:{
+            return t1->u.basic == t2->u.basic;
+        }
+        case ARRAY:{
+            return (t1->u.array.size == t2->u.array.size && isEqual(t1->u.array.elem, t2->u.array.elem));
+        }
+        case STRUCTURE:{
+            return (strcmp(t1->u.structure.name, t2->u.structure.name) == 0);
+        }
+        case FUNCTION:{
+            // 返回值
+            if(!isEqual(t1->u.function->returnType, t2->u.function->returnType)){
+                return 0;
+            }
+            // 参数列表
+            FieldList cur1 = t1->u.function->paramType;
+            FieldList cur2 = t2->u.function->paramType;
+            while(cur1 != NULL && cur2 != NULL){
+                if(!isEqual(cur1->type, cur2->type)){
+                    return 0;
+                }
+                cur1 = cur1->tail;
+                cur2 = cur2->tail;
+            }
+            if(cur1 != NULL || cur2 != NULL){               // 参数数目不相同
+                return 0;
+            }
+            return 1;
+        }
+        default:{
+            return 0;
+        }
+    }
 }
 
 void initSymbolTable(){
@@ -26,7 +75,7 @@ int insertSymbolTable(Symbol symbol){
         while (cur != NULL){
             if(strcmp(cur->name, symbol->name) == 0){           // 重名错误
                 if(cur->type->kind == FUNCTION && symbol->type->kind == FUNCTION){
-                    if(!cur->type->isEqual(symbol->type)){                                                          // 类型不同
+                    if(!isEqual(cur->type, symbol->type)){                                                          // 类型不同
                         return 19;
                     }else if(cur->type->u.function->isDefined == 0 && symbol->type->u.function->isDefined == 1){    // 先声明后定义
                         cur->type->u.function->isDefined = 1;
@@ -49,12 +98,117 @@ void serror(char* msg, int line, int errorType){
     printf("Error type %d at Line %d: %s.\n", errorType, line, msg);
 }
 
+void checkDef(){
+    for(int i = 0; i < TABLE_NUM; ++i){
+        Symbol cur = symbolTable[i];
+        while (cur != NULL){
+            if(cur->type->kind == FUNCTION && cur->type->u.function->isDefined == 0){
+                serror("Undefined function", cur->lineno, 18);
+            }
+            cur = cur->next;
+        }
+    }
+}
+
 void program(treeNode* root){
     if(root->firstChild != NULL){
         extDefList(root->firstChild);
-    }    
+        checkDef();
+    }
 }
 
-void extDefList(treeNode* root){
-    
+void extDefList(treeNode* parent){
+    if(parent == NULL || parent->firstChild == NULL)    return;
+    // ExtDefList: ExtDef ExtDefList
+    extDef(parent->firstChild);
+    extDefList(parent->firstChild->nextBrother);
+}
+
+void extDef(treeNode* parent){
+    if(parent == NULL)    return;
+    Type type = specifier(parent->firstChild);
+    if(strcmp(parent->firstChild->nextBrother->name, "SEMI") == 0){
+        // ExtDef: Specifier SEMI
+    }else if(strcmp(parent->firstChild->nextBrother->name, "ExtDecList") == 0){
+        // ExtDef: Specifier ExtDecList SEMI
+        extDecList(parent->firstChild->nextBrother, type);
+    }else if(strcmp(parent->firstChild->nextBrother->nextBrother->name, "SEMI") == 0){
+        // ExtDef: Specifier FunDec SEMI
+        funDec(parent->firstChild->nextBrother, type, 0);
+    }else{
+        // ExtDef: Specifier FunDec CompSt
+        funDec(parent->firstChild->nextBrother, type, 1);
+        compSt(parent->firstChild->nextBrother->nextBrother, type);
+    }
+}
+
+Type specifier(treeNode* parent){
+    if(parent == NULL)  return;
+    if(strcmp(parent->firstChild->name, "TYPE") == 0){
+        // Specifier: TYPE
+        Type type = (Type)malloc(sizeof(struct Type_));
+        type->kind = BASIC;
+        if(strcmp(parent->firstChild->text, "INT") == 0){
+            type->u.basic = INT;
+        }else{
+            type->u.basic = FLOAT;
+        }
+        return type;
+    }else{
+        // Specifier: StructSpecifier
+        return structSpecifier(parent->firstChild);
+    }
+}
+
+void extDecList(treeNode* parent, Type type){
+    if(parent == NULL)  return;
+    if(parent->firstChild->nextBrother == NULL){
+        // ExtDecList: VarDec
+        varDec(parent->firstChild, type);
+    }else{
+        // ExtDecList: VarDec COMMA ExtDecList
+        varDec(parent->firstChild, type);
+        extDecList(parent->firstChild->nextBrother->nextBrother, type);
+    }
+}
+
+void funDec(treeNode* parent, Type returnType, int isDef){
+    if(parent == NULL)  return;
+    Type funcType = (Type)malloc(sizeof(struct Type_));
+    funcType->kind = FUNCTION;
+    funcType->u.function = (Function)malloc(sizeof(struct Function_));
+    funcType->u.function->isDefined = isDef;
+    funcType->u.function->returnType = returnType;
+    funcType->u.function->paramNum = 0;
+    funcType->u.function->paramType = NULL;
+
+    Symbol func = newSymbol(parent->firstChild->text, funcType, parent->firstChild->lineno);
+    if(parent->firstChild->nextBrother->nextBrother->nextBrother == NULL){
+        // FunDec: ID LP RP
+    }else{
+        // FunDec: ID LP VarList RP
+        funcType->u.function->paramType = varList(parent->firstChild->nextBrother->nextBrother);
+        FieldList cur = funcType->u.function->paramType;
+        while (cur != NULL){
+            funcType->u.function->paramNum++;
+            cur = cur->tail;
+        }
+    }
+    insertSymbolTable(func);
+}
+
+void compSt(treeNode* parent, Type type){
+
+}
+
+Type structSpecifier(treeNode* parent){
+
+}
+
+void varDec(treeNode* parent, Type type){
+
+}
+
+FieldList varList(treeNode* parent){
+
 }
--- Lab/Lab2/hashtable.h
+++ Lab/Lab2/hashtable.h
@@ -10,8 +10,10 @@ typedef struct Symbol_* Symbol;
 
 enum Kind { BASIC, ARRAY, STRUCTURE, FUNCTION };
 
+int isEqual(Type t1, Type t2);                                      // 比较Type是否相等
+
 struct Type_{
-    Kind kind;
+    enum Kind kind;
     union{
         // 基本类型
         enum { INT, FLOAT } basic;
@@ -22,39 +24,12 @@ struct Type_{
         // 函数
         Function function;
     }u;
-    bool isEqual(Type f){
-        if(kind != f->kind){
-            return false;
-        }
-        switch(kind){
-            case BASIC:{
-                return u.basic == f->u.basic;
-            }
-            case ARRAY:{
-                return (u.array.size == f->u.array.size && u.array.elem->isEqual(f->u.array.elem));
-            }
-            case STRUCTURE:{
-                return (strcmp(u.structure.name, f->u.structure.name) == 0);
-            }
-            case FUNCTION:{
-                return u.function->isEqual(f->u.function);
-            }
-            default:{
-                return false;
-            }
-        }
-    }
 };
 
 struct FieldList_{
     char* name;             // 域的名字
     Type type;              // 域的类型
     FieldList tail;         // 下一个域
-    // bool isEqual(FieldList f){
-    //     if(type->){
-            
-    //     }
-    // }
 };
 
 struct Function_{
@@ -62,39 +37,30 @@ struct Function_{
     FieldList paramType;    //　参数类型
     int paramNum;           //　参数数量
     int isDefined;          //  是否被定义过
-    bool isEqual(Function f){
-        if(!returnType->isEqual(f->returnType)){
-            return false;
-        }
-        FieldList cur1 = paramType;
-        FieldList cur2 = f->paramType;
-        while(cur1 != NULL && cur2 != NULL){
-            if(!cur1->type->isEqual(cur2->type)){
-                return false;
-            }
-            cur1 = cur1->tail;
-            cur2 = cur2->tail;
-        }
-        if(cur1 != NULL || cur2 != NULL){               // 参数数目不相同
-            return false;
-        }
-        return true;
-    }
 };
 
 struct Symbol_{
     char* name;             // 变量名称
     Type type;              // 类型
+    int lineno;             // 行数
     Symbol next;            // 指向下一个节点的指针
-    Symbol_(char* Name, Type Type, Symbol Next){
-        strcpy(name, Name);
-        
-    }
 };
+
+Symbol newSymbol(char* name, Type type, int lineno);
 
 void initSymbolTable();
 int insertSymbolTable(Symbol symbol);
 void serror(char* msg, int line, int errorType);
 
+void checkDef();
+
 void program(treeNode* root);
-void extDefList(treeNode* root);
+void extDefList(treeNode* parent);
+void extDef(treeNode* parent);
+Type specifier(treeNode* parent);
+void extDecList(treeNode* parent, Type type);
+void funDec(treeNode* parent, Type returnType, int isDef);
+void compSt(treeNode* parent, Type type);
+Type structSpecifier(treeNode* parent);
+void varDec(treeNode* parent, Type type);
+FieldList varList(treeNode* parent);
